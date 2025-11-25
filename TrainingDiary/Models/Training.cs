c6b620f64ce08d447525523ed621d5051e0e2231
kovecsdavid1 <kovecsdavid05@gmail.com>
2025-11-25 12:10:07 +0100

    Model & services

---
+++ TrainingDiary/Models/Training.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using SQLite;
+
+namespace TrainingDiary.Models;
+
+public partial class Training : ObservableObject
+{
+    [ObservableProperty]
+    [property: PrimaryKey]
+    [property: AutoIncrement]
+    public int id;
+
+    [ObservableProperty]
+    public string name;
+
+    [ObservableProperty]
+    public string trainedRegion;
+
+    [ObservableProperty]
+    public string type;
+
+    [ObservableProperty]
+    public string intensivity;
+
+    [ObservableProperty]
+    public DateTime startTime;
+
+    [ObservableProperty]
+    public DateTime endTime;
+
+
+    public Training GetCopy()
+    {
+        return (Training)this.MemberwiseClone();
+    }
+}
---
+++ TrainingDiary/Services/DurationCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+using TrainingDiary.Models;
+
+namespace TrainingDiary.Services;
+
+public class DurationCalculator : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is Training training)
+        {
+            TimeSpan duration = training.EndTime - training.StartTime;
+            if (duration.TotalSeconds < 0)
+                duration = TimeSpan.Zero;
+
+            return $"{(int)duration.TotalHours} hour(s) {duration.Minutes} minute(s)";
+        }
+        return "No data";
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return Binding.DoNothing;
+    }
+}
